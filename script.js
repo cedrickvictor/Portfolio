a21f05bb2c7360b0d011d079749bf0ed4078a863
Grinel <94983012+cedrickvictor@users.noreply.github.com>
2021-12-16 11:28:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
-    
     $('#menu').click(function(){
-        $(this).toggleclass('fa-times');
-        $('header').toggleclass('toggle');
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle'); 
+    });
+
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+        if($(window).scrollTop() > 0){
+        	$('.top').show();
+
+        }else{
+        	$('.top').hide();
+        }
 
     });
     
-    $(window).on('scroll load',function(){
-        $('#menu').removeclass('fa-times');
-        $('header').removeclass('toggle');
-    });
+    // // $('a[href*="#"]').on('click',function(e){
+    // //     e.preventDefault();
+    // //     $('html, body').animate({
+
+    // //         scrollTop : $($(this).attr('href')).offset().top,
+
+    // //     },
+    // //      500,
+    // //      'linear'
+    // //     );
+
+    // });
+
 });
